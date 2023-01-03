df7eb908c46fcf4265b40e3361c5d7fae8be3614
EuanWorth <62507060+EuanWorth@users.noreply.github.com>
2023-01-03 17:25:27 +0000

    Read the Brief

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> myInts = new ArrayList<Integer>(List.of(1,2,3,4,5,6,7,8,9,10));
+        int sum1 = 0;
+        for (int i = 0; i < myInts.size(); i++) {
+            sum1 += myInts.get(i);
+        }
+        float avg1 = (float)sum1/myInts.size();
+
+        int sum2 = 0;
+        for(int myInt: myInts) {
+            sum2 += myInt;
+        }
+        float avg2 = (float)sum2 / myInts.size();
+
+        int sum3 = 0;
+        Iterator<Integer> MyIntIt = myInts.iterator();
+        while(MyIntIt.hasNext()) {
+            sum3 += MyIntIt.next();
+        }
+        float avg3 = (float)sum3 / myInts.size();
+
+        System.out.println("For Loop: " + avg1 + "\nFor Each Loop: " + avg2 + "\nIterator: " + avg3);
     }
 }
